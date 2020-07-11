86d7113c94a49443b2d46eee0027b059d0211be6
François Weber <f.weber@qwants-macbook-pro.home>
2020-07-11 11:25:54 +0200

    feat: Added copyPath

---
+++ copypath/copypath.py
@@ -0,0 +1,19 @@
+#! /usr/local/bin/python3
+import click
+import os
+from subprocess import run
+
+
+@click.command()
+@click.argument(
+    "path", type=click.Path(resolve_path=True), required=False, default=None
+)
+def cli(path):
+    path_or_dir = os.getcwd()
+    if path is not None:
+        path_or_dir = os.path.join(path_or_dir, path)
+    run("pbcopy", universal_newlines=True, input=path_or_dir)
+
+
+if __name__ == "__main__":
+    cli()
