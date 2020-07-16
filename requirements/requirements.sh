09fb34cd751a020a9666b3536160fc8af671ee55
François Weber <f.weber@MacBook-Pro-de-Francois.local>
2020-07-16 11:34:05 +0200

    feat: Used nana rather than vim

--- requirements/requirements.sh
+++ requirements/requirements.sh
@@ -25,3 +25,4 @@ rm -rf fonts
 # Git global alias
 git config --global alias.lg 'log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
 git config --global alias.co checkout
+git config --global core.editor "nano"
